f75ee82355c45d27dea802432f40f5f40b5b9e67
CesarOli <cesaraleoliveira@gmail.com>
2023-07-22 19:29:02 -0300

    Criação duas funções testes post.

--- hello.py
+++ hello.py
@@ -17,6 +17,14 @@ def teste():
 def show_user_profile(Cesar):
     return 'Seja bem vindo,  %s ' % Cesar
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % 8
+
+@app.route('/post2/<int:post_id>')
+def show_post2(post_id):
+    return 'Post %d' % 13
+
 if __name__ == '__main__':
     app.run(debug=True)
 
