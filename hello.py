06971b64d55526c009e748bc3c537efac05090c6
CesarOli <cesaraleoliveira@gmail.com>
2023-07-22 18:55:27 -0300

    Teste função user.

--- hello.py
+++ hello.py
@@ -13,6 +13,10 @@ def hello():
 def teste():
     return 'Testando Flask'
 
+@app.route('/user/<Cesar>')
+def show_user_profile(Cesar):
+    return 'Seja bem vindo,  %s ' % Cesar
+
 if __name__ == '__main__':
     app.run(debug=True)
 
