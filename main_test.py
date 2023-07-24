39288cc85441fe5c8e4fd89523ae215af9cd3f19
CesarOli <cesaraleoliveira@gmail.com>
2023-07-23 23:01:22 -0300

    Correção parâmetros com o msm nome das funções.

--- main_test.py
+++ main_test.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 def index():
     return 'Você está na página principal.'
 
-@app.route('/nome')
+@app.route('/login')
 def login():
-    return input('Digite seu nome: ')
+    return input('Digite seu login: ')
 
-@app.route('/user/<Cesar>')
+@app.route('/profile/<Cesar>')
 def profile(Cesar):
-    return input('Digite seu username: ')
+    return input('Digite seu profile: ')
 
 if __name__ == '__main__':
     app.run(debug=True)
