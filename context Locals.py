d00577e01f9ba5a5650807f992d73b280da0d8ce
CesarOli <cesaraleoliveira@gmail.com>
2023-07-25 23:03:13 -0300

    Adicionando teste para rota '/hello' usando método POST

--- context Locals.py
+++ context Locals.py
@@ -3,5 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-
+with app.test_request_context('/hello', method='POST'):
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+    
