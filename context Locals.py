f1fe9bea1fd29c300ef0c1ce30f77b4cb79d08af
CesarOli <cesaraleoliveira@gmail.com>
2023-07-26 15:19:24 -0300

    teste de método HTTP POST usando Flask request_contex

--- context Locals.py
+++ context Locals.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 with app.test_request_context('/hello', method='POST'):
     assert request.path == '/hello'
     assert request.method == 'POST'
-    
+
+with app.request_context(environ):
+    assert request.method == 'POST'
