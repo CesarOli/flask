188657098643219a2cd2c662220f032da8e0751c
CesarOli <cesaraleoliveira@gmail.com>
2023-07-27 19:22:27 -0300

    Adicionando segurança no arquivo enviado ao Flask.

--- file_uploads.py
+++ file_uploads.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def upload_file():
     if request.method == 'POST':
         f = request.files['the_file']
-        f.save('/var/www/uploads/uploaded_file.txt')
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
