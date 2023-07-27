e43b37acfea7a460d78fce017abccff3d1ac86b3
CesarOli <cesaraleoliveira@gmail.com>
2023-07-27 18:12:13 -0300

    Uso do atributo 'arg'.

--- object_request.py
+++ object_request.py
@@ -4,9 +4,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/login', methods=['POST', 'GET'])
+
 def login():
     error = None
+    searchword = request.args.get('key', '')
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             return log_the_user_in(request.form['username'])
@@ -14,6 +17,7 @@ def login():
             error = 'Invalid username/password'
 
         return render_template('login.html', error=error)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
