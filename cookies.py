c3b94c48c4a11239d1bc83226b0d73062ccddcf9
CesarOli <cesaraleoliveira@gmail.com>
2023-07-27 19:38:26 -0300

    Armazenamento do cookies do usuário.

--- cookies.py
+++ cookies.py
@@ -1,6 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
+'''@app.route('/')
+def index():
+    username = request.cookies.get('username')'''
+
 
 @app.route('/')
 def index():
-    username = request.cookies.get('username')
+    resp = make_response(render_template())
+    resp.set_cookie('username', 'the username')
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
