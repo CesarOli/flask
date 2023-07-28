752dd631b045cd3457ef224af6a9f06ce9888e8a
CesarOli <cesaraleoliveira@gmail.com>
2023-07-28 18:28:49 -0300

    Melhor Organização dos códigos.

--- hello.py
+++ hello.py
@@ -13,18 +13,6 @@ def hello():
 def teste():
     return 'Testando Flask'
 
-@app.route('/user/<Cesar>')
-def show_user_profile(Cesar):
-    return 'Seja bem vindo,  %s ' % Cesar
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post %d' % 8
-
-@app.route('/post2/<int:post_id>')
-def show_post2(post_id):
-    return 'Post %d' % 13
-
 @app.route('/projects/')
 def projects():
     return 'A página do projeto'
