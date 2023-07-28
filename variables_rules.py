22850fc3b5a0fd974860e487607883b4ca791216
CesarOli <cesaraleoliveira@gmail.com>
2023-07-28 18:42:32 -0300

    Regras variáveis e Modo Debug.

--- variables_rules.py
+++ variables_rules.py
@@ -1,3 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/user/<Cesar>')
+def show_user_profile(Cesar):
+    return 'Seja bem vindo,  %s ' % Cesar
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % 8
+
+@app.route('/post2/<int:post_id>')
+def show_post2(post_id):
+    return 'Post %d' % 13
+
+if __name__ == '__main__':
+    app.run(debug=True)
