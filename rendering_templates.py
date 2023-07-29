9061dcfdbd16f34c2caae394470d0d966c35ea32
CesarOli <cesaraleoliveira@gmail.com>
2023-07-29 17:29:01 -0300

    Renderização do .html sem sucessogit add rendering_templates.py

--- rendering_templates.py
+++ rendering_templates.py
@@ -1,12 +1,15 @@
-from flask import Flask, url_for
-from flask import render_template
+from flask import Flask,render_template, request
+from time import sleep
 
+#Falar com Fernando, criei o arquivo .html na pasta raiz Templates.
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name = None):
-    return render_template('hello.html', name = name)
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        return "Processando o login..."
+    else:
+        return render_template("hello.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
