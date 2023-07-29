f967c6b2a775035002475d9dc1f282ac451091cb
CesarOli <cesaraleoliveira@gmail.com>
2023-07-29 16:33:56 -0300

    correção no método GET das funções login() e show().

--- metodos.py
+++ metodos.py
@@ -1,12 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route ( '/login' ,  methods = [ 'GET' ,  'POST' ]) 
-def  login (): 
-    if  request . methods  ==  'POST' : 
-        do_the_login () 
-    else : 
-        show_the_login_form ()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "Processando o login..."
+    else:
+        return "Exibindo formulário de login..."
+
+@app.route('/show', methods=['GET', 'POST'])
+def show():
+    if request.method == 'POST':
+        return "Exibindo formulário de login..."
+    else:
+        return "Exibindo outra página..."
 
 if __name__ == '__main__':
     app.run(debug=True)
--- unique_urls_redirection_behavior.py
+++ unique_urls_redirection_behavior.py
@@ -2,3 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/projects/')
+def projects():
+    return 'A página do projeto'
+
+@app.route('/sobre')
+def sobre():
+    return 'A página sobre'
+
