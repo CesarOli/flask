279041a4d49c82a3b2e6a387c66f20e3b8eb053a
CesarOli <cesaraleoliveira@gmail.com>
2023-07-30 20:59:59 -0300

    Segunda opção de tratamento de erro na aplicação Flask.

--- about_response.py
+++ about_response.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
+'''@app.errorhandler(404)
+def not_found(error):
+    return render_template('erro.html'), 404'''
+
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('erro.html'), 404
+    resp = make_response(render_template('erro.html'), 404)
+    resp.headers['X-Something'] = 'A value'
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
