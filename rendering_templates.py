db690646f4e354668d893a261706f7ea7841942c
CesarOli <cesaraleoliveira@gmail.com>
2023-08-01 23:52:02 -0300

    Importação de novas libs, e novas URLs.

--- rendering_templates.py
+++ rendering_templates.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, url_for
 from time import sleep
 
-#Falar com Fernando, criei o arquivo .html na pasta raiz Templates.
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello/', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
         return "Processando o login..."
     else:
-        return render_template("hello.html")
+        return render_template('hello.html',name='Filano', caminho=url_for('static', filename='teste.txt'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
