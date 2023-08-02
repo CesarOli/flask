a62634ee50e0d3db16414818e86c3535cc7de18f
CesarOli <cesaraleoliveira@gmail.com>
2023-08-02 00:00:10 -0300

    Substituição de nome e número, por variaveis.

--- variables_rules.py
+++ variables_rules.py
@@ -1,17 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/user/<Cesar>')
-def show_user_profile(Cesar):
-    return 'Seja bem vindo,  %s ' % Cesar
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'Seja bem vindo,  %s ' % username
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    return 'Post %d' % 8
+    return 'Post %d' % post_id
 
 @app.route('/post2/<int:post_id>')
 def show_post2(post_id):
-    return 'Post %d' % 13
+    return 'Post %d' % post_id
 
 if __name__ == '__main__':
     app.run(debug=True)
