723a0cae07d3f501226ca58ae5e01a40918c0abe
CesarOli <cesaraleoliveira@gmail.com>
2023-08-02 22:47:30 -0300

    Regras Variaves, e Argumentos de palavras chaves.

--- rendering_templates.py
+++ rendering_templates.py
@@ -1,14 +1,18 @@
 from flask import Flask,render_template, request, url_for
 from time import sleep
+from datetime import date
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def hello():
+@app.route('/hello/<user>', methods=['GET', 'POST'])
+def hello(user):
     if request.method == 'POST':
         return "Processando o login..."
     else:
-        return render_template('hello.html',name='Filano', caminho=url_for('static', filename='teste.txt'))
+        return render_template('hello.html',
+                               data=date.today(),                               
+                               name=user,
+                               caminho=url_for('static', filename='teste.txt'))
 
 if __name__ == '__main__':
     app.run(debug=True)
