e38ddf237e3eba705a9905bc3e2724b5f6a449c6
CesarOli <cesaraleoliveira@gmail.com>
2023-08-04 00:01:39 -0300

    Import do módulo 'request'.

--- rendering_templates.py
+++ rendering_templates.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 @app.route('/hello/<user>', method=['POST', 'GET'])
 def hello(user):
     if request.methods == 'POST':
+        return "Processando seu login...Por favor, aguarde!!"
