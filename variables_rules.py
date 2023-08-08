c43816e18b5520d92ccc96284ed454095ea8656a
CesarOli <cesaraleoliveira@gmail.com>
2023-08-08 00:08:07 -0300

    Treino da 'variables rules.

--- variables_rules.py
+++ variables_rules.py
@@ -13,5 +13,10 @@ def show_post(post_id):
 def show_post2(post_id):
     return 'Post %d' % post_id
 
+@app.route('/barbearia/<endereco>')
+def barbearia(endereco):
+    return 'Rua Baubuena de Souza, 4 sala 19'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
