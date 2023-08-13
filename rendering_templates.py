b38786d9a169c838ea0a0984daf4bb7b13489fea
CesarOli <cesaraleoliveira@gmail.com>
2023-08-12 23:45:38 -0300

    Mensagem de retorno ao acessar a requisicao

--- rendering_templates.py
+++ rendering_templates.py
@@ -3,13 +3,14 @@ from datetime import date
 
 app = Flask(__name__)
 @app.route('/login', methods= ['POST', 'GET'])
-def login(login):
+def login():
     if request.method == 'POST':
-        return ''
+        return 'Testando método POST'
     else:
-        
+        return 'O Método não foi requisitado'
     
-@app.route('/hello/<user>', methods=['POST', 'GET'])
+    
+'''@app.route('/hello/<user>', methods=['POST', 'GET'])
 def hello(user):
     if request.method == 'POST':
         return "Processando seu login, seus dados são: " + request.form['idade'] + request.form['sexo'] + request.form['bairro']
@@ -22,6 +23,6 @@ def hello(user):
         return render_template('hello.html',
                            data=date.today(), 
                            name=user,
-                           caminho=url_for('static',filename='teste.txt'))
+                           caminho=url_for('static',filename='teste.txt'))'''
 if __name__=='__main__':
     app.run(debug=True)
