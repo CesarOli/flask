9bc84037f68f65d7e67068ebaece2a9cc55e2c52
CesarOli <cesaraleoliveira@gmail.com>
2023-08-20 23:43:44 -0300

    Sessões no Frame Flask

--- session.py
+++ session.py
@@ -1,3 +1,17 @@
 from flask import Flask, session, redirect, url_for, escape, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logado na sessao como %s' % escape(session['username'])
+    return 'Você ainda não esta logado!!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        
+if __name__ == '__main__':
+    app.run(debug=True)
